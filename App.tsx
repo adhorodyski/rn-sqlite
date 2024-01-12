70a78616e534af89e1fe996626a4f2780d42d9f3
Adam Horodyski <ad.horodyski@gmail.com>
2024-01-12 13:13:07 +0100

    fix: add root suspense boundary

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import {NavigationContainer} from '@react-navigation/native';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {queryClient} from './src/lib/queryClient';
 import {RootStack} from './src/RootStack';
+import {Suspense} from 'react';
 
 export default function App() {
   return (
     <NavigationContainer>
       <QueryClientProvider client={queryClient}>
-        <RootStack />
+        <Suspense>
+          <RootStack />
+        </Suspense>
       </QueryClientProvider>
     </NavigationContainer>
   );
