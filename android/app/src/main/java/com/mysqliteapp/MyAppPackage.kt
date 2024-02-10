fda9bd6a38e07d382c903ea7988f3d0b1704eafc
Adam Horodyski <ad.horodyski@gmail.com>
2024-02-10 19:02:18 +0100

    feat: add a package containing the calendar module

---
+++ android/app/src/main/java/com/mysqliteapp/MyAppPackage.kt
@@ -0,0 +1,18 @@
+package com.mysqliteapp
+
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+
+class MyAppPackage : ReactPackage {
+    override fun createNativeModules(
+        reactContext: ReactApplicationContext
+    ): MutableList<NativeModule> = listOf(CalendarModule(reactContext)).toMutableList()
+
+    override fun createViewManagers(
+        reactContext: ReactApplicationContext
+    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+}
