812e6aa7221edfe7523715bf4acb1db626767141
Adam Horodyski <ad.horodyski@gmail.com>
2024-02-17 14:58:21 +0100

    feat: redundant ui operations not really affecting performance (noise)

--- src/MessagesList.tsx
+++ src/MessagesList.tsx
@@ -1,7 +1,13 @@
 import {FlatList, Text, TouchableOpacity} from 'react-native';
 import {useSuspenseQuery} from '@tanstack/react-query';
+import dayjs from 'dayjs';
+import relativeTimePlugin from 'dayjs/plugin/relativeTime';
+import isTodayPlugin from 'dayjs/plugin/isToday';
 import {getChatMessages} from './queries/messages.native';
 import {messagesKeys} from './lib/keys';
+
+dayjs.extend(relativeTimePlugin);
+dayjs.extend(isTodayPlugin);
 
 interface Props {
   chatId: number;
@@ -20,8 +26,12 @@ export const MessagesList = ({chatId}: Props) => {
       renderItem={({item}) => (
         <TouchableOpacity key={item.id} style={{padding: 16}}>
           <Text>
-            {item.id}: {item.content}
+            {item.author_id === 1 ? 'You' : 'Someone'},{' '}
+            {dayjs(item.created_at).isToday()
+              ? dayjs(item.created_at).format('h:mm A')
+              : dayjs(item.created_at).fromNow(true)}
           </Text>
+          <Text>{item.content}</Text>
         </TouchableOpacity>
       )}
     />
