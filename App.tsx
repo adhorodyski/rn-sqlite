3970f0e7adbd5f2963d31120c9295f1df69aa3b7
Adam Horodyski <ad.horodyski@gmail.com>
2024-02-17 12:28:57 +0100

    chore: more featureProvider under /lib

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {Suspense} from 'react';
 import {RootStack} from './src/RootStack';
-import {FeatureProvider} from './src/featureProvider/FeatureProvider';
+import {FeatureProvider} from './src/lib/featureProvider/FeatureProvider';
 import {queryClient} from './src/lib/queryClient';
 
 export default function App() {
