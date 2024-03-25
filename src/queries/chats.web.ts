5ff2e7eb002596d8ce5a7fa0cd1674cf95f6167a
Adam Horodyski <ad.horodyski@gmail.com>
2024-03-25 18:05:30 +0100

    feat: implement getRecentChats for web

--- src/queries/chats.web.ts
+++ src/queries/chats.web.ts
@@ -1,22 +1,35 @@
 import {db} from '../lib/db.web';
 import type {ChatWithLastMessage} from '../lib/types';
 
-// TODO: Implement using Dexie
 export const getRecentChats = async () => {
-  // const chats = await db.kv.where('key').startsWith('chat_').toArray();
-  // const recentMessages = await db.kv
-  //   .where('key')
-  //   .startsWith('message_')
-  //   .toArray();
+  const keys = await db.kv.toCollection().primaryKeys();
 
-  // console.log(chats);
+  const chatsKeys = keys.filter(key => key.startsWith('chat_'));
+  const chats = await db.kv.bulkGet(chatsKeys);
 
-  return Promise.resolve([
-    {
-      id: 0,
-      title: 'test',
-      last_message: 'placeholder',
-      last_message_author_email: 'placeholder',
-    },
-  ] as ChatWithLastMessage[]);
+  const chatsWithLastMessage = await Promise.all(
+    chats.map(async chat => {
+      const messages = await db.kv
+        .toCollection()
+        .filter(msg => msg.chat_id === chat.id)
+        .toArray();
+
+      const lastMessage = messages.sort((a, b) =>
+        a.created_at > b.created_at ? -1 : 1,
+      )[0];
+
+      const lastMessageAuthor = await db.kv.get(
+        `user_${lastMessage.author_id}`,
+      );
+
+      return {
+        id: chat.id,
+        title: chat.title,
+        last_message: lastMessage.content,
+        last_message_author_email: lastMessageAuthor.email,
+      };
+    }),
+  );
+
+  return chatsWithLastMessage as ChatWithLastMessage[];
 };
