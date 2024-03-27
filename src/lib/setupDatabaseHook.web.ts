ee73a1f5372d34ff8647def6d841001e22a0b711
Adam Horodyski <ad.horodyski@gmail.com>
2024-03-27 17:07:17 +0100

    fix: indexeddb updates on web with storagemutated static event

--- src/lib/setupDatabaseHook.web.ts
+++ src/lib/setupDatabaseHook.web.ts
@@ -1,23 +1,12 @@
 import type {QueryClient} from '@tanstack/react-query';
-import {db} from './db.web';
+import Dexie from 'dexie';
 import {databaseQueryHook} from './databaseQueryHook';
 
 export const setupDatabaseHook = (queryClient: QueryClient) => {
-  db.kv.hook('creating', function (keyChanged) {
-    this.onsuccess = function () {
-      databaseQueryHook(keyChanged, queryClient);
-    };
-  });
-
-  db.kv.hook('updating', function (_, keyChanged) {
-    this.onsuccess = function () {
-      databaseQueryHook(keyChanged, queryClient);
-    };
-  });
-
-  db.kv.hook('deleting', function (keyChanged) {
-    this.onsuccess = function () {
-      databaseQueryHook(keyChanged, queryClient);
-    };
+  Dexie.on('storagemutated', function (parts) {
+    // TODO: This is a very naive implementation.
+    // Here's a one we can reuse later: https://github.com/dexie/Dexie.js/blob/f7d3708b5adc48fc9faf8a6c5fd85cb1042081d8/src/live-query/live-query.ts#L88
+    const keyChanged = Object.values(parts).find(part => part?.to)?.to;
+    databaseQueryHook(keyChanged, queryClient);
   });
 };
